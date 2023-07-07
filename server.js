df0680543d63462b0fc589e9f74d3143bca9706a
Ori Hendel <orihendel@Oris-MBP.lan>
2023-07-07 14:34:58 +0300

    save to db

--- server.js
+++ server.js
@@ -15,9 +15,32 @@ app.get('/', (req, res) => {
 app.post("/submit", upload.array("file"), uploadFiles);
 
 function uploadFiles(req, res) {
-    console.log(req.body);
-    console.log ("=================================");
-    console.log(req.files);
+    // console.log(req.body);
+    // console.log ("=================================");
+    // console.log(req.files);
+    // console.log ("=================================");
+
+    const MongoClient = require('mongodb').MongoClient;
+    const url = "mongodb+srv://orisolhendel:UJAy4xH95Gy5F6iR@cluster0.r22vgmy.mongodb.net/?retryWrites=true&w=majority";
+    
+    MongoClient.connect(url, function(err, db) {
+        if (err) {
+            throw err;
+        }
+        var dbo = db.db("markable");
+        console.log (dbo);
+        const myobj = { time: Date.now(), file: req.files[0], result: req.body.res };
+        console.log (myobj);
+        dbo.collection("selfies").insertOne(myobj, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            console.log(res);
+            console.log("1 document inserted");
+            db.close();
+        });
+    });
+
     res.json({ message: "Successfully uploaded files" });
 }
 
