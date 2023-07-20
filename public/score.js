cd2b1007bb172a752f9c86b7685ac60662ebb379
Ori Hendel <orihendel@Oris-MacBook-Pro.local>
2023-07-20 13:50:59 +0300

    initial score calc

---
+++ public/score.js
@@ -0,0 +1,122 @@
+const res = '{"request_id":"1689333902,47f5c5aa-32bd-49af-adbb-e85f8be88880","result":{"skin_age":{"value":46},"eye_pouch":{"value":0,"confidence":0.90306324},"dark_circle":{"value":1,"confidence":1},"dark_circle_severity":{"value":2,"confidence":1},"forehead_wrinkle":{"value":0,"confidence":0.7546025},"crows_feet":{"value":0,"confidence":0.79794335},"eye_finelines":{"value":1,"confidence":0.9778863},"eye_finelines_severity":{"value":2,"confidence":0.9339217},"glabella_wrinkle":{"value":0,"confidence":0.8563811},"nasolabial_fold":{"value":1,"confidence":0.7463967},"nasolabial_fold_severity":{"value":0,"confidence":0.29434815},"skin_type":{"skin_type":3,"details":[{"value":0,"confidence":0.19186819},{"value":0,"confidence":0.32414582},{"value":0,"confidence":0.06267304},{"value":0,"confidence":0.4213129}]},"pores_forehead":{"value":0,"confidence":1},"pores_left_cheek":{"value":0,"confidence":1},"pores_right_cheek":{"value":0,"confidence":1},"pores_jaw":{"value":0,"confidence":1},"blackhead":{"value":0,"confidence":1},"skintone_ita":{"ITA":7.855978,"skintone":5},"skin_hue_ha":{"HA":44.429485,"skin_hue":2},"acne":{"rectangle":[],"confidence":[],"polygon":[]},"mole":{"rectangle":[{"left":1885,"top":1151,"width":35,"height":44}],"confidence":[0.5147376],"polygon":[[{"x":1912,"y":1185},{"x":1898,"y":1189},{"x":1895,"y":1187},{"x":1891,"y":1176},{"x":1892,"y":1164},{"x":1895,"y":1161},{"x":1906,"y":1159},{"x":1912,"y":1162}]]},"brown_spot":{"rectangle":[{"left":2172,"top":3454,"width":28,"height":45},{"left":1805,"top":2282,"width":31,"height":45},{"left":1471,"top":2025,"width":37,"height":55},{"left":2309,"top":3093,"width":25,"height":36},{"left":2086,"top":2682,"width":35,"height":49},{"left":2291,"top":2820,"width":26,"height":32},{"left":2580,"top":2404,"width":26,"height":35},{"left":1445,"top":3158,"width":35,"height":42},{"left":2011,"top":718,"width":36,"height":43},{"left":2379,"top":3301,"width":26,"height":34},{"left":1326,"top":2462,"width":31,"height":55},{"left":2093,"top":2841,"width":33,"height":46},{"left":2119,"top":2677,"width":25,"height":33},{"left":2034,"top":2839,"width":29,"height":39},{"left":1481,"top":3040,"width":31,"height":41},{"left":1403,"top":3369,"width":33,"height":51},{"left":2051,"top":3449,"width":28,"height":49},{"left":2017,"top":2806,"width":24,"height":35},{"left":1569,"top":3307,"width":40,"height":53},{"left":2590,"top":1456,"width":44,"height":73},{"left":2103,"top":3539,"width":26,"height":41},{"left":1404,"top":2903,"width":35,"height":49},{"left":2014,"top":3388,"width":28,"height":38},{"left":2521,"top":2361,"width":33,"height":51},{"left":2618,"top":2054,"width":38,"height":81},{"left":1766,"top":3237,"width":30,"height":45},{"left":1691,"top":3417,"width":30,"height":39},{"left":1419,"top":2672,"width":39,"height":56}],"confidence":[0.64280224,0.56010073,0.52778053,0.46541741,0.46454406,0.464285,0.45538667,0.45026073,0.43891934,0.4345116,0.4325108,0.42166102,0.39861086,0.38633558,0.37211475,0.36453676,0.35432184,0.34781626,0.3390343,0.3358953,0.32982084,0.3209981,0.31687114,0.31503567,0.3066501,0.30532703,0.30305576,0.301259],"polygon":[[{"x":2183,"y":3493},{"x":2178,"y":3488},{"x":2177,"y":3477},{"x":2185,"y":3456},{"x":2194,"y":3468},{"x":2196,"y":3479},{"x":2193,"y":3489}],[{"x":1817,"y":2282},{"x":1827,"y":2291},{"x":1828,"y":2302},{"x":1814,"y":2326},{"x":1805,"y":2326},{"x":1805,"y":2282}],[{"x":1507,"y":2025},{"x":1505,"y":2045},{"x":1496,"y":2066},{"x":1479,"y":2070},{"x":1477,"y":2057},{"x":1481,"y":2042},{"x":1493,"y":2028}],[{"x":2326,"y":3122},{"x":2318,"y":3123},{"x":2309,"y":3112},{"x":2309,"y":3096},{"x":2329,"y":3100},{"x":2331,"y":3110}],[{"x":2113,"y":2721},{"x":2108,"y":2723},{"x":2086,"y":2710},{"x":2087,"y":2688},{"x":2102,"y":2684},{"x":2109,"y":2688},{"x":2116,"y":2709}],[{"x":2314,"y":2838},{"x":2309,"y":2843},{"x":2295,"y":2845},{"x":2291,"y":2841},{"x":2291,"y":2834},{"x":2300,"y":2823}],[{"x":2600,"y":2433},{"x":2588,"y":2431},{"x":2584,"y":2421},{"x":2586,"y":2409},{"x":2595,"y":2408},{"x":2603,"y":2420},{"x":2603,"y":2429}],[{"x":1468,"y":3167},{"x":1473,"y":3177},{"x":1468,"y":3191},{"x":1456,"y":3199},{"x":1445,"y":3199},{"x":1447,"y":3171},{"x":1454,"y":3165}],[{"x":2038,"y":749},{"x":2025,"y":754},{"x":2011,"y":746},{"x":2011,"y":718},{"x":2020,"y":718},{"x":2038,"y":726},{"x":2041,"y":736}],[{"x":2397,"y":3333},{"x":2389,"y":3328},{"x":2384,"y":3316},{"x":2386,"y":3309},{"x":2391,"y":3305},{"x":2404,"y":3308},{"x":2404,"y":3327}],[{"x":1346,"y":2512},{"x":1339,"y":2512},{"x":1326,"y":2495},{"x":1326,"y":2472},{"x":1340,"y":2463},{"x":1347,"y":2470},{"x":1349,"y":2477},{"x":1349,"y":2501}],[{"x":2125,"y":2841},{"x":2116,"y":2876},{"x":2106,"y":2881},{"x":2098,"y":2878},{"x":2095,"y":2871},{"x":2102,"y":2852},{"x":2113,"y":2841}],[{"x":2139,"y":2697},{"x":2129,"y":2698},{"x":2123,"y":2693},{"x":2124,"y":2684},{"x":2130,"y":2679},{"x":2137,"y":2681},{"x":2141,"y":2691}],[{"x":2045,"y":2845},{"x":2053,"y":2848},{"x":2059,"y":2855},{"x":2056,"y":2864},{"x":2044,"y":2875},{"x":2040,"y":2873},{"x":2039,"y":2853}],[{"x":1511,"y":3080},{"x":1488,"y":3067},{"x":1493,"y":3040},{"x":1511,"y":3040}],[{"x":1423,"y":3418},{"x":1414,"y":3417},{"x":1407,"y":3412},{"x":1407,"y":3384},{"x":1411,"y":3380},{"x":1426,"y":3382},{"x":1430,"y":3390},{"x":1428,"y":3410}],[{"x":2064,"y":3491},{"x":2056,"y":3487},{"x":2055,"y":3467},{"x":2061,"y":3450},{"x":2070,"y":3449},{"x":2075,"y":3477},{"x":2072,"y":3486}],[{"x":2032,"y":2836},{"x":2023,"y":2832},{"x":2022,"y":2811},{"x":2031,"y":2810},{"x":2035,"y":2814},{"x":2036,"y":2829}],[{"x":1590,"y":3358},{"x":1584,"y":3356},{"x":1572,"y":3341},{"x":1579,"y":3315},{"x":1584,"y":3310},{"x":1595,"y":3316},{"x":1601,"y":3337},{"x":1598,"y":3354}],[{"x":2633,"y":1456},{"x":2633,"y":1484},{"x":2627,"y":1516},{"x":2624,"y":1522},{"x":2619,"y":1523},{"x":2613,"y":1521},{"x":2600,"y":1507},{"x":2599,"y":1480},{"x":2607,"y":1456}],[{"x":2116,"y":3543},{"x":2124,"y":3552},{"x":2123,"y":3569},{"x":2113,"y":3575},{"x":2103,"y":3574},{"x":2110,"y":3546}],[{"x":1421,"y":2912},{"x":1427,"y":2916},{"x":1430,"y":2924},{"x":1426,"y":2944},{"x":1423,"y":2949},{"x":1404,"y":2951},{"x":1404,"y":2918},{"x":1409,"y":2911}],[{"x":2037,"y":3422},{"x":2026,"y":3417},{"x":2020,"y":3410},{"x":2020,"y":3396},{"x":2031,"y":3388},{"x":2036,"y":3391},{"x":2040,"y":3416}],[{"x":2538,"y":2411},{"x":2526,"y":2382},{"x":2527,"y":2364},{"x":2553,"y":2373},{"x":2553,"y":2387},{"x":2545,"y":2404}],[{"x":2652,"y":2134},{"x":2618,"y":2134},{"x":2618,"y":2064},{"x":2633,"y":2062},{"x":2640,"y":2070},{"x":2652,"y":2103}],[{"x":1785,"y":3274},{"x":1781,"y":3274},{"x":1772,"y":3263},{"x":1769,"y":3250},{"x":1771,"y":3237},{"x":1782,"y":3237},{"x":1789,"y":3247},{"x":1791,"y":3258}],[{"x":1694,"y":3455},{"x":1691,"y":3455},{"x":1691,"y":3417},{"x":1712,"y":3417},{"x":1713,"y":3442}],[{"x":1448,"y":2682},{"x":1449,"y":2694},{"x":1443,"y":2720},{"x":1419,"y":2727},{"x":1419,"y":2701},{"x":1436,"y":2683}]]},"closed_comedones":{"rectangle":[],"confidence":[],"polygon":[]},"acne_mark":{"rectangle":[],"confidence":[],"polygon":[]},"acne_nodule":{"rectangle":[],"confidence":[],"polygon":[]},"acne_pustule":{"rectangle":[],"confidence":[],"polygon":[]},"blackhead_count":3,"skintone":{"value":2,"confidence":0.4894698},"fine_line":{"forehead_count":0,"left_undereye_count":0,"right_undereye_count":0,"left_cheek_count":0,"right_cheek_count":0,"left_crowsfeet_count":0,"right_crowsfeet_count":0,"glabella_count":0},"wrinkle_count":{"forehead_count":0,"left_undereye_count":0,"right_undereye_count":0,"left_mouth_count":0,"right_mouth_count":0,"left_nasolabial_count":1,"right_nasolabial_count":0,"glabella_count":0,"left_cheek_count":0,"right_cheek_count":0,"left_crowsfeet_count":0,"right_crowsfeet_count":0},"oily_intensity":{"t_zone":{"area":0.35,"intensity":2},"left_cheek":{"area":0,"intensity":0},"right_cheek":{"area":0.66,"intensity":2},"chin_area":{"area":0.25,"intensity":1}},"enlarged_pore_count":{"forehead_count":7,"left_cheek_count":1,"right_cheek_count":0,"chin_count":15},"right_dark_circle_rete":{"value":0},"left_dark_circle_rete":{"value":0},"right_dark_circle_pigment":{"value":3},"left_dark_circle_pigment":{"value":2},"right_dark_circle_structural":{"value":0},"left_dark_circle_structural":{"value":2},"dark_circle_mark":{"left_eye_rect":{"left":1355,"top":1704,"width":459,"height":398},"right_eye_rect":{"left":2079,"top":1706,"width":480,"height":417}},"water":{"water_severity":7,"water_area":0.065,"water_forehead":{"area":0.037},"water_leftcheek":{"area":0.183},"water_rightcheek":{"area":0.027}},"rough":{"rough_severity":12,"rough_area":0.146,"rough_forehead":{"area":0.135},"rough_leftcheek":{"area":0.117},"rough_rightcheek":{"area":0.155},"rough_jaw":{"area":0.184}},"left_mouth_wrinkle_severity":{"value":0},"right_mouth_wrinkle_severity":{"value":0},"forehead_wrinkle_severity":{"value":0},"left_crows_feet_severity":{"value":0},"right_crows_feet_severity":{"value":0},"left_eye_finelines_severity":{"value":0},"right_eye_finelines_severity":{"value":0},"glabella_wrinkle_severity":{"value":0},"left_nasolabial_fold_severity":{"value":1},"right_nasolabial_fold_severity":{"value":0},"left_cheek_wrinkle_severity":{"value":0},"right_cheek_wrinkle_severity":{"value":0},"forehead_wrinkle_info":{"wrinkle_score":0,"wrinkle_severity_level":0,"wrinkle_norm_length":0,"wrinkle_norm_depth":0,"wrinkle_pixel_density":0,"wrinkle_area_ratio":0,"wrinkle_deep_ratio":0,"wrinkle_deep_num":0,"wrinkle_shallow_num":0},"left_eye_wrinkle_info":{"wrinkle_score":0,"wrinkle_severity_level":0,"wrinkle_norm_length":0,"wrinkle_norm_depth":0,"wrinkle_pixel_density":0,"wrinkle_area_ratio":0,"wrinkle_deep_ratio":0,"wrinkle_deep_num":0,"wrinkle_shallow_num":0},"right_eye_wrinkle_info":{"wrinkle_score":0,"wrinkle_severity_level":0,"wrinkle_norm_length":0,"wrinkle_norm_depth":0,"wrinkle_pixel_density":0,"wrinkle_area_ratio":0,"wrinkle_deep_ratio":0,"wrinkle_deep_num":0,"wrinkle_shallow_num":0},"left_crowsfeet_wrinkle_info":{"wrinkle_score":0,"wrinkle_severity_level":0,"wrinkle_norm_length":0,"wrinkle_norm_depth":0,"wrinkle_pixel_density":0,"wrinkle_area_ratio":0,"wrinkle_deep_ratio":0,"wrinkle_deep_num":0,"wrinkle_shallow_num":0},"right_crowsfeet_wrinkle_info":{"wrinkle_score":0,"wrinkle_severity_level":0,"wrinkle_norm_length":0,"wrinkle_norm_depth":0,"wrinkle_pixel_density":0,"wrinkle_area_ratio":0,"wrinkle_deep_ratio":0,"wrinkle_deep_num":0,"wrinkle_shallow_num":0},"glabella_wrinkle_info":{"wrinkle_score":0,"wrinkle_severity_level":0,"wrinkle_norm_length":0,"wrinkle_norm_depth":0,"wrinkle_pixel_density":0,"wrinkle_area_ratio":0,"wrinkle_deep_ratio":0,"wrinkle_deep_num":0,"wrinkle_shallow_num":0},"left_mouth_wrinkle_info":{"wrinkle_score":0,"wrinkle_severity_level":0,"wrinkle_norm_length":0,"wrinkle_norm_depth":0,"wrinkle_pixel_density":0,"wrinkle_area_ratio":0,"wrinkle_deep_ratio":0,"wrinkle_deep_num":0,"wrinkle_shallow_num":0},"right_mouth_wrinkle_info":{"wrinkle_score":0,"wrinkle_severity_level":0,"wrinkle_norm_length":0,"wrinkle_norm_depth":0,"wrinkle_pixel_density":0,"wrinkle_area_ratio":0,"wrinkle_deep_ratio":0,"wrinkle_deep_num":0,"wrinkle_shallow_num":0},"left_nasolabial_wrinkle_info":{"wrinkle_score":15,"wrinkle_severity_level":1,"wrinkle_norm_length":0.22674660709228833,"wrinkle_norm_depth":0.14610521804350154,"wrinkle_pixel_density":0.051373439053768,"wrinkle_area_ratio":0.009365543149326933,"wrinkle_deep_ratio":1,"wrinkle_deep_num":1,"wrinkle_shallow_num":0},"right_nasolabial_wrinkle_info":{"wrinkle_score":0,"wrinkle_severity_level":0,"wrinkle_norm_length":0,"wrinkle_norm_depth":0,"wrinkle_pixel_density":0,"wrinkle_area_ratio":0,"wrinkle_deep_ratio":0,"wrinkle_deep_num":0,"wrinkle_shallow_num":0},"left_cheek_wrinkle_info":{"wrinkle_score":0,"wrinkle_severity_level":0,"wrinkle_norm_length":0,"wrinkle_norm_depth":0,"wrinkle_pixel_density":0,"wrinkle_area_ratio":0,"wrinkle_deep_ratio":0,"wrinkle_deep_num":0,"wrinkle_shallow_num":0},"right_cheek_wrinkle_info":{"wrinkle_score":0,"wrinkle_severity_level":0,"wrinkle_norm_length":0,"wrinkle_norm_depth":0,"wrinkle_pixel_density":0,"wrinkle_area_ratio":0,"wrinkle_deep_ratio":0,"wrinkle_deep_num":0,"wrinkle_shallow_num":0},"score_info":{"dark_circle_score":54,"skin_type_score":31,"wrinkle_score":98,"oily_intensity_score":45,"pores_score":99,"blackhead_score":99,"acne_score":100,"sensitivity_score":30,"melanin_score":96,"water_score":93,"rough_score":88,"total_score":79,"pores_type_score":{"pores_forehead_score":99,"pores_leftcheek_score":100,"pores_rightcheek_score":100,"pores_jaw_score":98},"dark_circle_type_score":{"left_dark_circle_score":70,"right_dark_circle_score":84}},"left_eye_pouch_rect":{"left":1355,"top":1704,"width":459,"height":398},"right_eye_pouch_rect":{"left":2079,"top":1706,"width":480,"height":417},"melasma":{"value":1,"confidence":0.5211471},"freckle":{"value":0,"confidence":0.30378485},"image_quality":{"face_rect":{"left":1198,"top":721,"width":1621,"height":3122},"face_ratio":0.24859375,"hair_occlusion":0.17317946,"face_orientation":{"yaw":-4.9192834,"pitch":6.524694,"roll":0.56027937},"glasses":0},"sensitivity_type_v1":0},"face_rectangle":{"top":1337,"left":795,"width":2373,"height":2373},"error_code":0,"log_id":"357927"}';
+
+let scoreDebug = ""
+
+const debugScore = s => {
+    console.log (s);
+    scoreDebug += s + "<br/>";
+}
+
+const gradeScore = (x, input, output) => {
+    for (let i = 0; i < input.length; i++) {
+        if (x <= input[i]) {
+            return output[i];
+        }
+    }
+}
+
+const calctScore = (res, symptoms/*, test*/) => {
+
+    const jres_wrapper = (JSON.parse (res));
+    const jres = jres_wrapper.result;
+
+    const info = jres.score_info
+
+    debugScore (`symptoms: ${symptoms}`);
+
+    let water = info.water_score;
+    debugScore (`water: ${water}`)
+
+    let pores = info.pores_score;
+    debugScore (`pores: ${pores}`);
+
+    let melanin = info.melanin_score;
+    debugScore (`melanin: ${melanin}`);
+
+    let dark_circle = info.dark_circle_score;
+    debugScore (`dark_circle: ${dark_circle}`);
+
+    let sensitivity = info.sensitivity_score
+    debugScore (`sensitivity: ${sensitivity}`);
+
+    let eye_pouch_val = jres.eye_pouch.value;
+    let eye_pouch_conf = jres.eye_pouch.confidence;
+    debugScore (`eye_pouch_val: ${eye_pouch_val}`);
+    debugScore (`eye_pouch_conf: ${eye_pouch_conf}`);
+
+    let skin_type_val = jres.skin_type.skin_type;
+    let skin_type_conf = jres.skin_type.details[1].confidence;
+    debugScore (`skin_type_val: ${skin_type_val}`);
+    debugScore (`skin_type_conf: ${skin_type_conf}`);
+
+    const water_score = gradeScore (water, [20, 70, 100], [10, 5, 0]);
+    const pore_score = gradeScore (pores, [30, 70, 100], [0, 5, 10]);
+    const melanin_score = gradeScore (melanin, [30, 75, 100], [0, 5, 10]);
+    const dark_circle_score = gradeScore (dark_circle, [40, 100], [0, 10])
+    const sensitivity_score = gradeScore (sensitivity, [50, 80, 100], [0, 5, 10])
+    const eye_pouch_score = eye_pouch_val == 1 && eye_pouch_conf > 0.1 ? 10 : 0;
+    const skin_type_score = skin_type_val == 1 && skin_type_conf > 0.2 ? 10 : 0;
+
+    const factors = [water_score, pore_score, melanin_score, dark_circle_score, sensitivity_score, eye_pouch_score, skin_type_score];
+    const factors_names = ["water_score", "pore_score", "melanin_score", "dark_circle_score", "sensitivity_score", "eye_pouch_score", "skin_type_score"];
+
+    let total_score_relative =    factors.reduce((a, b) => a + b, 0)
+    const total_score_empiric = total_score_relative / factors.length * 10 
+
+    debugScore ("=================")
+    factors.forEach ((a, i) => debugScore (`${factors_names[i]}: ${a}`));
+    debugScore ("=================")
+    debugScore (`total_score_relative: ${total_score_relative}`);
+    debugScore ("=================")
+    debugScore (`total_score_empiric: ${total_score_empiric}`);
+
+    ///*test ++++++++++++++++++++++++++ */ total_score_relative = test;
+    let classification = "";
+
+    switch (symptoms) {
+        case 0:
+            classification = gradeScore (total_score_relative, [45, 70], ["D", "C"]);
+            break;
+        case 1:
+            classification = gradeScore (total_score_relative, [45, 70], ["D", "C"]);
+            break;
+        case 2:
+            classification = gradeScore (total_score_relative, [30, 40, 70], ["D", "C", "B"]);
+            break;
+        case 3:
+            classification = gradeScore (total_score_relative, [40, 45, 70], ["C", "B", "A"]);
+            break;
+        default: // 4 and above
+            classification = gradeScore (total_score_relative, [15, 45, 70], ["C", "B", "A"]);
+            break;
+    }
+    debugScore ("====================================================================")
+
+    debugScore (`class: ${classification}`);
+
+    return scoreDebug;
+}
+
+// debug:
+
+// for (let i = 0; i < 50; i++) {
+//     let symptoms = Math.floor(Math.random() * 6);
+//     let test = Math.floor(Math.random() * 71);
+
+//     debugScore (`symptoms: ${symptoms}` )
+//     debugScore (`test: ${test}`);
+
+//     calctScore (res, symptoms, test);
+
+
+//     debugScore ("====================================================================")
+//     debugScore ("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+//     debugScore ("====================================================================")
+
+// }
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -33,11 +33,10 @@ function uploadFiles(req, res) {
     fs.readFile(req.files[0].path, 'hex', function(err, imgData) {
         if (err) console.log (err);
         imgData = '\\x' + imgData;
-        client.query(`INSERT INTO Selfies (id, dt, selfie, results) values (DEFAULT, NOW(), ($1), '${req.body.res}')`,
+        client.query(`INSERT INTO Selfies (id, dt, selfie, results, selfiesDim) values (DEFAULT, NOW(), ($1), '${req.body.res}', '${req.body.dim}')`,
                            [imgData],
                            function(err) {
                                 if (err) throw err;
-                                console.log (res);
                                 client.end();
         });
       });
@@ -58,7 +57,7 @@ app.get('/show_db', function(req, res, next) {
 
     client.connect();
 
-    client.query('SELECT id, dt, results FROM Selfies ORDER BY dt DESC',
+    client.query('SELECT id, dt, results, selfiesDim FROM Selfies ORDER BY dt DESC',
                         function(err, readResult) {
                             if (err) console.log (err);
                             res.send(JSON.stringify(readResult));
@@ -67,69 +66,68 @@ app.get('/show_db', function(req, res, next) {
 });
 
 
-app.get('/show_pic', function(req, res, next) {
+
+app.get('/show_pic', async function(req, res, next) {
 
     const { Client } = require('pg');
     const fs = require('fs');
 
     // before goint to the db - see if our file is already in place:
 
-    let requestedFile = "";
+    
     fs.readdir(__dirname + '/public/tmp', function (err, files) {
         
         if (err) {
             return console.log('Unable to scan directory: ' + err);
         } 
         
-        files.forEach(function (file) {
+        for(let index = 0; index < files.length; index++){
 
-            console.log (file);
+            const file = files [index];
+            // TODO: use guid and not datatime for picture format
             if (file.startsWith (`${req.query.id}_`)) {
                 console.log ("file already in dir");
-                fileExists = true;
-                requestedFile = file;
-                return;
-            }
-        });
-    });
+                res.send (file); 
+                break;
+            } else if (index === files.length - 1) { // not found - take from db
 
-    if (requestedFile) {
-        req.send (requestedFile);
-        return;
-    }
+                console.log ("fetching from database");
 
-    const client = new Client({
-        connectionString: "postgres://hrdqkjutlzxtmz:ff4b0a7d5e00ee748c50a6ce93a662e9ea727d9f9abb11e13055b9a4c2a77ded@ec2-35-169-11-108.compute-1.amazonaws.com:5432/dc8f13lp2d9518",
-
-        ssl: {
-            rejectUnauthorized: false
-        }
-    });
-
-    client.connect();
-
-    client.query(`SELECT selfie, dt FROM Selfies WHERE id = ${req.query.id} `,
-                        function(err, readResult) {
-                            if (err) console.log (err);
-
-                            let imgName = "";
-                            try {
-                                imgName = req.query.id + "_" + readResult.rows[0].dt;
-                            } catch (e) {
-                                console.log (e);
-                            }
-
-                            let path = "";
-                            try {
-                                path = __dirname + `/public/tmp/${imgName}.jpg`;
-                            } catch (e) {
-                                console.log (e);
-                            }
-
-                            fs.writeFile(path, readResult.rows[0].selfie, { overwrite: false },err => {if (err) {console.log (err)}});
-                            
-                            res.send(`${imgName}.jpg`);
-                            client.end();
+                const client = new Client({
+                    connectionString: "postgres://hrdqkjutlzxtmz:ff4b0a7d5e00ee748c50a6ce93a662e9ea727d9f9abb11e13055b9a4c2a77ded@ec2-35-169-11-108.compute-1.amazonaws.com:5432/dc8f13lp2d9518",
+            
+                    ssl: {
+                        rejectUnauthorized: false
+                    }
+                });
+            
+                client.connect();
+            
+                client.query(`SELECT selfie, dt, selfiesDim FROM Selfies WHERE id = ${req.query.id} `,
+                                    function(err, readResult) {
+                                        if (err) console.log (err);
+            
+                                        let imgName = "";
+                                        try {
+                                            imgName = req.query.id + "_" + readResult.rows[0].dt;
+                                        } catch (e) {
+                                            console.log (e);
+                                        }
+            
+                                        let path = "";
+                                        try {
+                                            path = __dirname + `/public/tmp/${imgName}.jpg`;
+                                        } catch (e) {
+                                            console.log (e);
+                                        }
+            
+                                        fs.writeFile(path, readResult.rows[0].selfie, { overwrite: false },err => {if (err) {console.log (err)}});
+                                        
+                                        res.send(`${imgName}.jpg`);
+                                        client.end();
+                });
+            } 
+        };
     });
 });
 
