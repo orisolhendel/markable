650cd0ed06c9fbb9b238a813c1039a53858fcc33
Ori Hendel <orisolhendel@gmail.com>
2023-12-19 14:26:54 +0200

    loading gif test

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -11,3 +11,10 @@ moveUp ();
 const getAppMode = () => {
     return new URLSearchParams(window.location.search).get("mode");
 }
+
+const MARKABLE_DEVICES = {
+    web : Symbol("web"),
+    mobile: Symbol("mobile")
+  };
+
+const markable_device = (screen.width > 1000) ? MARKABLE_DEVICES.web : MARKABLE_DEVICES.mobile; 
